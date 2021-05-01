85801fce61df10968217210e98f3a4f76b4de87b
amitksms97 <amitksms97@gmail.com>
2021-05-01 18:32:58 +0530

    Add project files.

---
+++ LearningNullConditional/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LearningNullConditional
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = null;  
+            Person person = new Person(); //{ Name = "Tim" }; 
+
+            //Console.WriteLine(person?.Name?.Length);
+
+            Console.WriteLine(person?.Name?? "Not Set");
+
+            //if (person != null)
+            //{
+            //    Console.WriteLine(person.Name);
+            //}
+        }
+    }
+}
